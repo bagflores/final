d74e9e4af7a79aacb53f5e81838de3d6abef34de
Bruno Flores <floresbrunoag@gmail.com>
2024-10-22 15:36:26 -0300

    Añadí archivo llamado dedoneSacamostodo, que indica el seguimiento de la informacion proporcionada

---
+++ dedondeSacamostodo.js
@@ -0,0 +1,46 @@
+// Integrar GoogleMaps:
+// https://www.youtube.com/watch?v=HtLfFlqyiIs - (vueltero pero clara las condiciones y las extensiones)
+// Para el posible reparto de productos gracias al GoogleMaps:
+// https://www.youtube.com/watch?v=RL044Sgm0Ug
+// Validacion de datos:
+// https://www.youtube.com/watch?v=EdBZQ6IdlYs - con uso de autenticate / middleware etc....
+// https://www.youtube.com/watch?v=VMRgFfmv6j0 - con validatehelper - req - res - nes ...etc..
+// Uso correcto de Github:
+// https://www.youtube.com/watch?v=rAnn6vtLm90
+// https://www.youtube.com/watch?v=eeFE3WvtoDY
+
+// Datos Github: CREAR REPOSITORIO
+// Clonar repositorio a la maquina local:
+// git clone https://github.com/bagflores/final.git
+
+// Navegar (ir) al directorio del repositorio:
+// cd final
+// *para poder dale ordenes
+
+// Crear carpetas dentro del repo usado:
+// mkdir carpeta1 carpeta2
+// *donde carpeta1 y carpeta2 son los nombres de las carpetas
+// *mkdir backend frontend
+
+// Añadir un archivo temporal en cada carpeta porque no se pueden subir vacias:
+// *archivo temporal adentro (como un archivo .gitkeep) en caso de no tener creado archivos de uso
+// New-Item -Path "backend/.gitkeep" -ItemType File
+// New-Item -Path "frontend/.gitkeep" -ItemType File
+// *New-Item -Path backend\.gitkeep -ItemType File
+// New-Item -Path frontend\.gitkeep -ItemType File
+
+// Realiza un commit inicial de los cambios:
+// git add .
+
+// Commit de los cambios: Aqui podemos indicar que cambios utilizamos para que un administrador lo controle
+// git commit -m "Añadí las carpetas carpeta1 y carpeta2"
+// *git commit -m "Añadí las carpetas backend y frontend"
+
+// Subir cambios a GitHub:
+// git push origin main
+
+// Siempre Verificar en GitHub:
+// Ir al github
+
+
+
